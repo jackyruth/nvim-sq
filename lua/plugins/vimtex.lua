e27bbf03ec9192a25f28dd1d6237011dd7bfa2c4
Jacky Jiang <jackyjiang98@gmail.com>
2025-07-20 20:05:16 -0400

    disabled plugins instead of renaming file

--- lua/plugins/vimtex.lua
+++ lua/plugins/vimtex.lua
@@ -1,5 +1,6 @@
 return {
    "lervag/vimtex",
+   lazy = false,
    -- DO NOT lazy load, reverse search won't work
    -- Use init for configuration, don't use the more common "config", see
    -- https://github.com/lervag/vimtex?tab=readme-ov-file#installation
